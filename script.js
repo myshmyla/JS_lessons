2bec82f8c6832994b5298057d9b4a805238decfa
myshmyla <67440432+myshmyla@users.noreply.github.com>
2020-12-07 23:34:57 +0300

    урок 6 исправил

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 'use strict';
+let f1 = function () {
+  let randomNum = parseInt(Math.random() * 100);
+  console.log(randomNum);
 
-let randomNum = parseInt(Math.random() * 100);
-console.log(randomNum);
+  function guessNumber(userNum) {
+    userNum = prompt('Угадай число от 1 до 100');
 
-function guessNumber(userNum) {
-  userNum = prompt('Угадай число от 1 до 100');
-  
-  if (isNaN(userNum)) {
-    alert('Введи число!');
-    guessNumber(userNum);
+    if (isNaN(userNum)) {
+      alert('Введи число!');
+      guessNumber(userNum);
+    }
+
+    if (userNum === null) {
+      confirm('Игра окончена');
+      return;
+    }
+
+    if (userNum == randomNum) {
+      alert('Поздравляю, Вы угадали!!!');
+      return;
+    } else if (userNum > randomNum) {
+      alert('Загаданное число меньше');
+      guessNumber(userNum);
+    } else if (userNum < randomNum) {
+      alert('Загаданное число больше');
+      guessNumber(userNum);
+    }
   }
+  guessNumber();
+};
 
-  if (userNum === null) {
-    confirm ('Игра окончена')
-    return;
-  }
-
-  if (userNum == randomNum) {
-    alert('Поздравляю, Вы угадали!!!');
-    return;
-
-  } else if (userNum < randomNum) {
-    alert('Загаданное число меньше');
-    guessNumber(userNum);
-  
-  } else if (userNum > randomNum) {
-    alert('Загаданное число больше');
-    guessNumber(userNum);
-  }
-}
-
-guessNumber();
+f1();
